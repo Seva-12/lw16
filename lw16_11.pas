7bd0cb0bdc8df3eedcb8765158663c7dcc44f159
Seva <medved12zh@gmail.com>
2025-05-16 10:31:14 +0300

    16 lw

---
+++ lw16_11.pas
@@ -0,0 +1,34 @@
+PROGRAM CompMonth(INPUT, OUTPUT);
+
+USES
+  DateIO;
+VAr
+  Mo1, Mo2: Month;
+BEGIN
+  ReadMonth(INPUT, Mo1);
+  ReadMonth(INPUT, Mo2);
+  IF (Mo1 = NoMonth) OR (Mo2 = NoMonth) 
+  THEN
+    WRITELN('Неверные исходные данные')
+  ELSE
+    IF (Mo1 = Mo2)
+    THEN
+      BEGIN
+        WRITE('Оба месяца ');
+        WriteMonth(OUTPUT, Mo1)
+      END  
+    ELSE
+      IF (Mo2 > Mo1)
+      THEN 
+        BEGIN  
+          WriteMonth(OUTPUT, Mo1);
+          WRITE(' следует за ');
+          WriteMonth(OUTPUT, Mo2)
+        END
+      ELSE
+        BEGIN
+          WriteMonth(OUTPUT, Mo1);
+          WRITE(' предшествует ');
+          WriteMonth(OUTPUT, Mo2)
+        END
+END. 
